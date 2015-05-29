ab00029c03e861e3fbb38dd439e3ad10d19832d7
Vinay Selvaraj <vinay@selvaraj.com>
2015-05-29 14:19:44 -0400

    Removed sudo

--- chef/solo.sh
+++ chef/solo.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e /usr/bin/chef-solo ]
 then
-  curl -L https://www.chef.io/chef/install.sh | sudo bash
+  curl -L https://www.chef.io/chef/install.sh |  bash
 fi
 
 /usr/bin/chef-solo -c /chef/solo.rb -j /chef/solo.json
