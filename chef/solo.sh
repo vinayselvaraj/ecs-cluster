63a4729eb3747a2d1e6d1e633cc2c5dfe9594249
Vinay Selvaraj <vinay@selvaraj.com>
2015-06-02 10:37:06 -0400

    Changed to install chefdk

--- chef/solo.sh
+++ chef/solo.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e /usr/bin/chef-solo ]
 then
-  curl -L https://www.chef.io/chef/install.sh |  bash
+  rpm -Uvh https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.0-1.el6.x86_64.rpm
 fi
 
 /usr/bin/chef-solo -c /chef/solo.rb -j /chef/solo.json
